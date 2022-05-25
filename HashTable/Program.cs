c41423f5977b54bbd1b138b13d424f482ae18772
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-26 01:03:14 +0530

    [Sunny] Add : Find Frequncy Of Words In A Sentence

---
+++ HashTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HashTable;
+class program
+{
+    public static void Main(string[] args)
+    {
+        MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+        string words = "to be or not to be";
+        string[] arr = words.Split(' ');
+        LinkedList<string> checkforduplication = new LinkedList<string>();
+        foreach (string element in arr) // to -> be
+        {
+            int count = 0;
+            foreach (string match in arr) //to->be->or->not->to-> be-> 
+            {
+                if (element == match)
+                {
+                    count++;//1->2
+                    if (checkforduplication.Contains(element))
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (!checkforduplication.Contains(element))
+            {
+                checkforduplication.AddLast(element);
+                hash.Add(element, count);//(to,2)
+            }
+        }
+        hash.Display();
+    }
+}
