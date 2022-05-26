1887cd9b27b166a3576f5cd38c40f4c6d7ab2cb2
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-26 16:11:42 +0530

    [Sunny] Add : Remove Avoidable Words From The Phrase

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,14 +7,15 @@ class program
         MyMapNode<string, int> hash = new MyMapNode<string, int>(5);//
         Console.WriteLine("Welcome To The Hash Table");
         bool end = true;
-        Console.WriteLine("1. Find Frequency Of Words\n2. Frequency Of Words In Large Paragraph Phrase \n3. End the Program\n");
+        Console.WriteLine("1. Find Frequency Of Words\n2. Frequency Of Words In Phrase and Remove\n3. End the Program\n");
         while (end == true)
         {
-            Console.WriteLine("Take an option to execute");
+            Console.WriteLine("Take an option to execute : ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
+                    MyMapNode<string, int> hash1 = new MyMapNode<string, int>(5);//size 5
                     string words = "to be or not to be";
                     string[] arr = words.Split(' ');
                     LinkedList<string> checkForDuplication = new LinkedList<string>();
@@ -35,13 +36,15 @@ class program
                         if (!checkForDuplication.Contains(element))
                         {
                             checkForDuplication.AddLast(element);
-                            hash.Add(element, count);//(to,2)
+                            hash1.Add(element, count);//(to,2)
                         }
                     }
-                    hash.Display();
+                    hash1.Display();
                     break;
                 case 2:
-                    string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                    MyMapNode<string, int> hash2 = new MyMapNode<string, int>(5);
+                    string phrase = "Paranoids are not paranoid because they are " +
+                        "paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
                     string[] arr1 = phrase.Split(' ');
                     LinkedList<string> checkDuplication = new LinkedList<string>();
                     foreach (string element in arr1)
@@ -63,9 +66,14 @@ class program
                             continue;
                         }
                         checkDuplication.AddLast(element);
-                        hash.Add(element, count);
+                        hash2.Add(element, count);
                     }
-                    hash.Display();
+                    int freq = hash2.Get("avoidable");
+                    Console.WriteLine("Frequency of the word Avoidable : " + freq);
+                    hash2.Remove("avoidable");
+                    freq = hash2.Get("avoidable");
+                    Console.WriteLine("Frequency of the word Avoidable after removing : " + freq);
+                    hash2.Display();
                     break;
                 case 3:
                     end = false;
@@ -75,6 +83,5 @@ class program
                     break;
             }
         }
-
     }
 }
